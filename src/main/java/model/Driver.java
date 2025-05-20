580473217535e7f86301643a91aa8cda7b7a672d
IT24103555 <IT24103555@my.sliit.lk>
2025-05-20 07:27:50 +0530

    Update Driver.java

--- src/main/java/model/Driver.java
+++ src/main/java/model/Driver.java
@@ -1,7 +1,61 @@
 package com.example.taxi.model;
 
 public class Driver {
-    private String id, name, vehicle;
+    private String id;
+    private String name;
+    private String vehicle;
     private double rating;
-    // getters & setters
+
+    
+    public Driver(String id, String name, String vehicle, double rating) {
+        this.id = id;
+        this.name = name;
+        this.vehicle = vehicle;
+        this.rating = rating;
+    }
+
+    
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", vehicle='" + vehicle + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
 }
